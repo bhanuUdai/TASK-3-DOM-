1b73c3add3f17f8fc839f003f115a8a0aeac81dc
BHANU <udaibhanu2602@gmail.com>
2022-08-01 02:18:42 +0530

    changing inner text and style  of add items

---
+++ dom.js
@@ -0,0 +1,31 @@
+//console.dir(document)
+//console.log(document.domain)
+//console.log(document.URL)
+//console.log(document.title)
+//document.title='helo'
+//console.log(document.title)
+//console.log(document.URL)
+//console.log(document.doctype);
+//console.log(document.head)
+
+//console.log(document.body);
+//console.log(document.all)
+//console.log(document.all[10])
+//document.all[10].textContent='hello'
+//console.log(document.images)
+
+//console.log(document.getElementById('header-title'))
+//const headTitle=document.getElementById('header-title');
+//headTitle.textContent='helo'
+//const title=document.getElementById('yoboii')
+//title.textContent='hnji'
+//headTitle.innerText='hloo'
+//console.log(headTitle.innerText)
+
+const headtitle=document.getElementById('main-header');
+headtitle.style.borderBottom='3px solid blue'
+const add=document.all[14]
+add.innerText='ADD ITEMS'
+add.style.color='green'
+//console.log(add)
+console.log(document.all)
