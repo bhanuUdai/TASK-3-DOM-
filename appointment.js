0666e4c2377200bede379f8ff33de0124174bed0
BHANU <udaibhanu2602@gmail.com>
2022-08-07 15:08:51 +0530

    Task 13 Edit/Delete button

--- appointment.js
+++ appointment.js
@@ -5,6 +5,7 @@ let unord=document.querySelector('.for_ul');
 form.addEventListener('submit',onsubmit);
 unord.addEventListener('click',onclick);
 
+
 function onsubmit(e)
 {
     e.preventDefault();
@@ -12,16 +13,7 @@ function onsubmit(e)
     let Name=document.querySelector('#Name');
     let Mail=document.querySelector('#Mail');
 
-    let obj={
-        name:`${Name.value}`,
-        mail:`${Mail.value}`
-    }
-
-    let obj_serialize=JSON.stringify(obj);
-    localStorage.setItem(`Object ${Name.value}`,obj_serialize);
-
-    let obj_deserialize=JSON.parse(localStorage.getItem(`Object ${Name.value}`));
-    console.log(obj_deserialize);
+    
 
 
     let li=document.createElement('li');
@@ -29,13 +21,50 @@ function onsubmit(e)
     let text=document.createTextNode(`Name: ${Name.value} Mail: ${Mail.value}`);
     li.appendChild(text);
 
+
+
+    let edit=document.createElement('button');
+    edit.className='edit_btn';
+    edit.style='color:orange;'
+    let text2=document.createTextNode('EDIT');
+    edit.appendChild(text2);
+    li.append(edit)
+   
+    
+
     let btn=document.createElement('button');
     btn.className="for_btn";
+    btn.style='color:orange;'
     let text1=document.createTextNode('DELETE');
     btn.appendChild(text1);
     li.appendChild(btn);
 
+    
+
+    if(!localStorage.getItem(`Object ${Name.value}`))  //if Object ${Name.value} is not present in local storage then print only
+   {
     unord.appendChild(li);
+   }
+
+
+   //creating object of input fields
+   let obj={
+    name:`${Name.value}`,
+    mail:`${Mail.value}`
+   }
+
+// converting into string
+
+    let obj_serialize=JSON.stringify(obj);
+    localStorage.setItem(`Object ${Name.value}`,obj_serialize);
+
+//converting back to object
+    let obj_deserialize=JSON.parse(localStorage.getItem(`Object ${Name.value}`));
+    console.log(obj_deserialize);
+
+
+    Name.value='';
+    Mail.value='';
 
 }
 
@@ -46,6 +75,17 @@ function onclick(e)
     {
         let Li=e.target.parentElement;
         unord.removeChild(Li);
+        localStorage.clear(`Object ${Name.value}`)
+    }
+
+    if(e.target.classList.contains('edit_btn'))
+    {
+        let Li =e.target.parentElement;
+        unord.removeChild(Li);
     }
  
 }
+
+
+
+
